dd1baa1ae0d0da3c509dcdb2108012ec2988f730
Praneswar G <praneswarganesan@gmail.com>
2024-07-27 14:20:04 +0530

    TLEMPDASH

--- frontend/src/components/dashboards/TeamLeadDashboard/components/ManageTeam.js
+++ frontend/src/components/dashboards/TeamLeadDashboard/components/ManageTeam.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import SidePanel from '../TeamLeadSidePanel';
+// src/components/dashboards/TeamLeadDashboard/components/ManageTeam.js
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import TeamLeadSidePanel from '../TeamLeadSidePanel';
 
 const ManageTeam = () => {
   return (
-    <div>
-        <SidePanel />
-        ManageTeam</div>
-  )
-}
+    <Box sx={{ display: 'flex' }}>
+      <TeamLeadSidePanel />
+      <Box sx={{ p: 3, flexGrow: 1 }}>
+        <Typography variant="h4" gutterBottom>
+          Manage Team
+        </Typography>
+        <Calendar />
+      </Box>
+    </Box>
+  );
+};
 
-export default ManageTeam
+export default ManageTeam;
